b4a6b75eb3a2b00627b960bc39e005e6b0b5b94e
“Zack <zpeters@ebmcopy.com>
2017-11-29 11:38:28 -0600

    Added live reloading

--- builds/development/js/script.js
+++ builds/development/js/script.js
@@ -77,7 +77,7 @@ $ = require('jquery');
 
 (fill = function(item) {
   return $('.tagline').append("" + item);
-})('The most CREATIVE minds in Art');
+})('The most creative minds in Art');
 
 fill;
 
--- components/scripts/tagline.js
+++ components/scripts/tagline.js
@@ -4,6 +4,6 @@ $ = require('jquery');
 
 (fill = function(item) {
   return $('.tagline').append("" + item);
-})('The most CREATIVE minds in Art');
+})('The most creative minds in Art');
 
 fill;
